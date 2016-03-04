b41ec900647b442f5592557de20476aba4c1e555
JacksonWong <jackson.cw.wong@gmail.com>
2016-03-03 20:20:59 -0500

    added products to the controller

--- public/js/controller/app.js
+++ public/js/controller/app.js
@@ -3,5 +3,38 @@
 angular.module('shopApp', []);
 
 var shopAppController = function($scope){
-	$scope.products = 
+	$scope.products = [{
+		"id": 0,
+		"name": "Portraiture Pendant",
+		"vendor": "Mimiko and Company",
+		"categories": ["accessories"],
+		"description": "A beautiful, elegant piece.",
+		"price": 85.00,
+		"onSale": false,
+		"thumb": "img/img-pendant.png",
+		"quantityInStock": 5
+	},
+	{
+		"id": 1,
+		"name": "Cufflinks",
+		"vendor": "Chromeo",
+		"categories": ["accessories"],
+		"description": "Rich heritage cufflinks, pure silver.",
+		"price": 200.00,
+		"onSale": false,
+		"thumb": "img/img-cufflinks.png",
+		"quantityInStock": 10
+	},
+	{
+		"id": 2,
+		"name": "Neilson Carginan",
+		"vendor": "Crux and Crucible",
+		"categories": ["outerwear"],
+		"description": "Rich heritage cufflinks, pure silver.",
+		"price": 200.00,
+		"onSale": false,
+		"thumb": "img/img-cardigan.png",
+		"quantityInStock": 10
+	}];
+	
 }
